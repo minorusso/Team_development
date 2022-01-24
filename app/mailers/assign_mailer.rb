53112ce492dfbc1619e485784caa629232e12d0f
minorusso <gumbomb1@gmail.com>
2022-01-24 17:47:12 +0900

    メール機能実装&エラー修正

--- app/mailers/assign_mailer.rb
+++ app/mailers/assign_mailer.rb
@@ -6,4 +6,8 @@ class AssignMailer < ApplicationMailer
     @password = password
     mail to: @email, subject: I18n.t('views.messages.complete_registration')
   end
+  def assignment_of_authority_mail(email)
+    @email = email
+    mail to: @email, subject: I18n.t('views.messages.complete_change')
+  end
 end
